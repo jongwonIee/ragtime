281ce991514951d52bfea89a1f7845d41c06f869
lee <ljw3728@gmail.com>
2017-01-20 22:35:20 +0900

    initial commit

---
+++ app/controllers/ragtimes_controller.rb
@@ -0,0 +1,11 @@
+class RagtimesController < ApplicationController
+  def index
+    @string = "migrate"
+    @answer = ""
+    # the variable @answer should be "Ragtime" to clear the quest ( current, its "" )
+    # use ruby methods and the given variable @string = "migrate" to make @answer to "Ragtime"
+    # do not just type @answer = "Ragtime" (cheating)
+    # your code here
+  end
+end
+#if you need hint see README.md line26
